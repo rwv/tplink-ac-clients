c12d8c9f10b5013400cb004e38658e4fb5cecf75
Seedgou <i@zczc.cz>
2024-01-12 15:32:26 +0800

    add divider in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import { useWhereStore } from '@/stores/where'
 import SettingsCollapse from '@/components/settings/SettingsCollapse.vue'
 import APClients from '@/components/clients/APClients.vue'
 import ClientHeader from '@/components/clients/ClientsHeader.vue'
+import { NDivider } from 'naive-ui'
 
 const whereStore = useWhereStore()
 
@@ -15,7 +16,9 @@ onMounted(async () => {
 <template>
   <main>
     <ClientHeader />
+    <n-divider />
     <APClients />
+    <n-divider />
     <SettingsCollapse />
   </main>
 </template>
