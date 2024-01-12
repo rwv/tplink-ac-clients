f88b4880b4c379d2c2c201f0dcc1738ce4ceb1dc
Seedgou <i@zczc.cz>
2024-01-12 13:45:18 +0800

    add global theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import {
+  useOsTheme,
+  darkTheme,
+  NConfigProvider,
+  NGlobalStyle,
+  NMessageProvider,
+  zhCN,
+  dateZhCN
+} from 'naive-ui'
+import { computed } from 'vue'
+
+const osThemeRef = useOsTheme()
+const theme = computed(() => (osThemeRef.value === 'dark' ? darkTheme : null))
 </script>
 
 <template>
-  <RouterView />
+  <n-config-provider :theme="theme" :locale="zhCN" :date-locale="dateZhCN">
+    <n-global-style />
+    <n-message-provider>
+      <RouterView />
+    </n-message-provider>
+  </n-config-provider>
 </template>
