0ddbae3b235846fc12e1792c8828dff3fbe1a753
Seedgou <i@zczc.cz>
2024-01-15 11:11:08 +0800

    lint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useWhereStore } from '@/stores/where'
-import SettingsCollapse from '@/components/settings/SettingsCollapse.vue'
 import APClients from '@/components/clients/APClients.vue'
 import ClientHeader from '@/components/clients/ClientsHeader.vue'
 import { NDivider } from 'naive-ui'
