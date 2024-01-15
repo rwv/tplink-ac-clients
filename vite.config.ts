7bb88d5e7be114a5aac9ef9e3046c5c3db2be08a
Seedgou <i@zczc.cz>
2024-01-15 11:00:59 +0800

    add icon and pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,46 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "favicon.svg", "apple-touch-icon.png"],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Where',
+        short_name: 'Where',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
